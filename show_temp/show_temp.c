3b3b48d3f9092f04524604400e38ac46ab633ec4
Viktor Viskov <carrergt@gmail.com>
2024-10-25 15:40:41 +0200

    add show temp on lcd app

--- show_temp/show_temp.c
+++ show_temp/show_temp.c
@@ -3,6 +3,7 @@
 // include drivers
 #include "../lcd/lcd.h"
 #include "../temp/temp.h"
+#include "../lcd/rbg.h"
 
 // include utils
 #include "../utils/converting.h"
@@ -11,9 +12,12 @@
 int main( void ) {
     char buf[16];
     int lcd_device = init_lcd_device(LCD_BUS, LCD_ADR);
+    int rgb_device = init_rgb_device(RGB_BUS, RGB_ADR);
     int temp_device = init_temp_device(TEMP_BUS, TEMP_ADR);
 
     clear_lcd(lcd_device);
+    enable_rgb(rgb_device);
+    reset_rgb(rgb_device);
 
     // loop for show temp
     while (1) {
@@ -25,6 +29,27 @@ int main( void ) {
         float_to_string(temp_celc, buf);
         display_string_first_line(lcd_device, buf);
         display_string_second_line(lcd_device, "Hello World");
+
+        // set red led if temp > 30
+        if (temp_celc >= 30.0 ){
+            set_red(rgb_device, 255);
+        } else {
+            set_red(rgb_device, 0);
+        }
+
+        // set green led if temp < 30 and > 5
+        if (temp_celc < 30.0 && temp_celc > 5.0 ){
+            set_green(rgb_device, 255);
+        } else {
+            set_green(rgb_device, 0);
+        }
+
+        // set blue led if temp < 5
+        if (temp_celc <= 5.0 ){
+            set_blue(rgb_device, 255);
+        } else {
+            set_blue(rgb_device, 0);
+        }
         usleep(500000); // 0.5 sec
     }
 
