cffe721ba1b9fd50f79af586c1dc882e12212930
Viktor Viskov <carrergt@gmail.com>
2024-10-31 10:11:26 +0100

    Refactoring to module mcp9808

--- kernel_module/mcp9808.c
+++ kernel_module/mcp9808.c
@@ -3,13 +3,14 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
+#include <linux/i2c.h>
+#include <linux/slab.h>
 
-#include "temp.h"
 
-#define DEVICE_NAME "char_device"
+#define DEVICE_NAME "mcp9808_temp"
 #define CLASS_NAME "custom_devices"
-#define HELLO_STRING "Char device\n"
-
+#define TEMP_BUS        2
+#define TEMP_ADR        0x18
 
 static int major_number;
 static struct class* custom_devices = NULL;
@@ -17,6 +18,41 @@ static struct device* char_device = NULL;
 
 static int device_open(struct inode *inode, struct file *file) { return 0; }
 static int device_release(struct inode *inode, struct file *file) { return 0; }
+
+
+static int get_temp(void) {
+    struct i2c_adapter *adapter;
+    u8 buf[2];
+    int ret;
+    int raw_data;
+    int temp;
+
+    struct i2c_client *client = kzalloc(sizeof(*client), GFP_KERNEL);
+
+    adapter = i2c_get_adapter(TEMP_BUS);
+    if (!adapter) {
+        printk(KERN_ALERT "Failed to get I2C adapter %d\n", TEMP_BUS);
+        return -ENODEV;
+    }
+
+    // printk(KERN_ALERT "Failed to get I2C client for address %d\n");
+    client->adapter = adapter;
+    client->addr = TEMP_ADR;
+    if (!client) {
+        printk(KERN_ALERT "Failed to get I2C client for address %d\n", TEMP_ADR);
+        i2c_put_adapter(adapter);
+        return -ENODEV;
+    }
+
+    ret = i2c_master_recv(client, buf, 2);
+    if (ret < 0) {
+        printk(KERN_ALERT "Failed to read data: %d\n", ret);
+    }
+    raw_data = (buf[0] << 8) | buf[1];
+    temp = (raw_data >> 4) & 0xff;
+
+    return temp;
+}
 
 static ssize_t device_read(struct file *file, char __user *buffer, size_t len, loff_t *offset) {
     int temp = get_temp();
@@ -44,7 +80,7 @@ static int __init char_init(void) {
     // Device register
     major_number = register_chrdev(0, DEVICE_NAME, &fops);
     if (major_number < 0) {
-        printk(KERN_ALERT "Char device failed to register a major number\n");
+        printk(KERN_ALERT "MCP9808 failed to register a major number\n");
         return major_number;
     }
 
@@ -52,7 +88,7 @@ static int __init char_init(void) {
     custom_devices = class_create(THIS_MODULE, CLASS_NAME);
     if (IS_ERR(custom_devices)) {
         unregister_chrdev(major_number, DEVICE_NAME);
-        printk(KERN_ALERT "Failed to register device class\n");
+        printk(KERN_ALERT "MCP9808 failed to register device class\n");
         return PTR_ERR(custom_devices);
     }
 
@@ -61,11 +97,11 @@ static int __init char_init(void) {
     if (IS_ERR(char_device)) {
         class_destroy(custom_devices);
         unregister_chrdev(major_number, DEVICE_NAME);
-        printk(KERN_ALERT "Failed to create the device\n");
+        printk(KERN_ALERT "MCP9808 failed to create the device\n");
         return PTR_ERR(char_device);
     }
 
-    printk(KERN_INFO "Char device created successfully\n");
+    printk(KERN_INFO "MCP9808 device created successfully\n");
     return 0;
 }
 
@@ -75,7 +111,7 @@ static void __exit char_exit(void) {
     class_unregister(custom_devices);
     class_destroy(custom_devices);
     unregister_chrdev(major_number, DEVICE_NAME);
-    printk(KERN_INFO "Char device unregistered\n");
+    printk(KERN_INFO "MCP9808 device unregistered\n");
 }
 
 module_init(char_init);
@@ -84,5 +120,5 @@ module_exit(char_exit);
 // modul info
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Viktor Viskov");
-MODULE_DESCRIPTION("A simple Linux char device that returns a string");
+MODULE_DESCRIPTION("A simple Linux MCP9808 device that returns a temperature");
 MODULE_VERSION("1.0");
